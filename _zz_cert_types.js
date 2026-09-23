d4b786a9002c3eacaa6e85d46d2cf4b3ba72deed
ShonaB29 <shonab2903@gmail.com>
2026-09-23 23:48:24 +0530

    test files deleted

--- _zz_cert_types.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-require('dotenv').config();
-(async () => {
-  const client = new Client({ connectionString: process.env.DATABASE_URL });
-  await client.connect();
-  const res = await client.query(`SELECT * FROM certificate_types LIMIT 20`);
-  console.log(res.rows);
-  await client.end();
-})();
